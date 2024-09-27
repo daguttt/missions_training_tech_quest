c63830237566c9ffad129fd34887563b37c57d06
daguttt <dagutmu667@gmail.com>
2024-09-27 14:50:25 -0500

    chore: initial commit

---
+++ src/main/java/com/riwi/missions_training_tech_quest/MissionsTrainingTechQuestApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.missions_training_tech_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionsTrainingTechQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionsTrainingTechQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/missions_training_tech_quest/MissionsTrainingTechQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.missions_training_tech_quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionsTrainingTechQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
