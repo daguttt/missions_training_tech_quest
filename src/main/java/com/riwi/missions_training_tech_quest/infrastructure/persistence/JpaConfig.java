06a5f03b31eb218c475c2fc650385910aee6374d
daguttt <dagutmu667@gmail.com>
2024-10-02 18:29:47 -0500

    wip: add initial configuration to enable auditing in the app

---
+++ src/main/java/com/riwi/missions_training_tech_quest/infrastructure/persistence/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.riwi.missions_training_tech_quest.infrastructure.persistence;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
